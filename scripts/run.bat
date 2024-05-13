be1ae18c5bf9185c4c5ee47a8acf0b138deac184
Linus Zaiser <st172540@stud.uni-stuttgart.de>
2024-05-13 21:30:50 +0200

    Fix path issues in run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,7 +5,7 @@ ECHO Activating Python Virtual Environment
 call .\venv\Scripts\activate.bat
 
 rem Starting the Backend Server
-start python .\Flask_Server\FALCC_New\app.py
+start python .\Flask_Server\FairCR\app.py
 
 rem Starting Frontend Development Server
 cd angular-project
